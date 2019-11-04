8d173f9c0b3b0d6da049bd1c0f4f0de9e4c9b2b0
Wills Blake <richwblake@gmail.com>
2019-11-04 19:40:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,9 @@
 # Write your code here.
 def line( line )
+  new_line_string = String.new
   if line.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{line.each_with_index{ |index, name|  }}"
+    puts "The line is currently: #{line.each_with_index{ |index, name| "" }}"
   end
 end
