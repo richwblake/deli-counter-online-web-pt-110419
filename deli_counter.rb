62ec8b40484d36e5e4ebc74461385518e2f6b7a2
Wills Blake <richwblake@gmail.com>
2019-11-04 19:42:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line( line )
   if line.empty?
     puts "The line is currently empty."
   else
-    line.each_with_index{ |index, name| new_line_string << "#{index}. #{name}"}
+    line.each_with_index{ |index, name| new_line_string << "#{index}. #{name} "}
   end
 end
